50591ed9fa6cbea2c81587c1c9961ed0cc14f6cf
minthuhlaing <minthuhlaing786@gmail.com>
2024-09-24 17:03:17 +0630

    starting project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>How fast can you catch me?</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <div v-if="isPlaying">
+    <Block :delay="delay" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from "./components/Block.vue"
 export default {
   name: 'App',
+  data(){
+    return{
+      isPlaying:false,
+      delay:null,
+    }
+  },
   components: {
-    HelloWorld
+    Block
+  },
+  methods:{
+   start(){
+    this.isPlaying = true;
+    this.delay=2000+Math.random()*5000;
+   }
   }
 }
 </script>
---
+++ src/components/Block.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="block">
+    Click {{ delay }}
+  </div>
+</template>
+
+<script>
+export default {
+props:["delay"],
+}
+</script>
+
+<style>
+.block{
+  width: 300px;
+  height: 300px;
+  padding: 20px;
+  margin: 50px auto;
+  background: #2c3e50;
+  color:white
+}
+</style>
